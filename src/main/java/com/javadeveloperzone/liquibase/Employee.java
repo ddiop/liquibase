8cb8f0bdaf169823589e193517d8b5271da6a859
ddiop <djibiop82@hotmail.com>
2019-10-15 22:35:57 +0200

    first commit

---
+++ src/main/java/com/javadeveloperzone/liquibase/Employee.java
@@ -0,0 +1,47 @@
+package com.javadeveloperzone.liquibase;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="employee_table")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="employee_id")
+	private Integer employeeId;
+	
+	@Column(name="employee_name")
+	private String employeeName;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="salary")
+	private Double salary;
+	
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+}
